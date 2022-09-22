ec4c628f6f50cca912f90134ec7a7415f8b34768
jwalters8 <111069960+WasianMD@users.noreply.github.com>
2022-09-22 09:38:10 -0400

    Create BreadTester.java

---
+++ BreadTester.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+public class BreadTester{
+  public static void main(String[] args) throws FileNotFoundException{
+    
+    Scanner input = newScanner(new File("data.txt"));
+    
+    
+  }
+}
